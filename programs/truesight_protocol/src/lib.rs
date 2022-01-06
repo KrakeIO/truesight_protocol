cc7bdd3c2daf9a52f8a8b3ddcd31eea89791be8e
davidberget@gmail.com <davidberget@gmail.com>
2022-01-06 11:03:55 -0600

    basic asset and direction account

--- programs/truesight_protocol/src/lib.rs
+++ programs/truesight_protocol/src/lib.rs
@@ -5,10 +5,27 @@ declare_id!("Fg6PaFpoGXkYsidMpWTK6W2BeZ7FEfcYkg476zPFsLnS");
 #[program]
 pub mod truesight_protocol {
     use super::*;
-    pub fn initialize(_ctx: Context<Initialize>) -> ProgramResult {
+    pub fn initialize(ctx: Context<Initialize>, asset_name: String) -> ProgramResult {
+        let prediction_account = &mut ctx.accounts.prediction_account;
+        prediction_account.direction = 1;
+        prediction_account.asset = asset_name;
+
         Ok(())
     }
 }
 
 #[derive(Accounts)]
-pub struct Initialize {}
+pub struct Initialize<'info> {
+    #[account(init, payer = user, space = 64 + 64)]
+    pub prediction_account: Account<'info, PredictionAccount>,
+    #[account(mut)]
+    pub user: Signer<'info>,
+    pub system_program: Program<'info, System>,
+}
+
+
+#[account]
+pub struct PredictionAccount {
+    pub direction: u64,
+    pub asset: String,
+}
--- tests/truesight_protocol.ts
+++ tests/truesight_protocol.ts
@@ -1,17 +1,34 @@
 import * as anchor from '@project-serum/anchor';
 import { Program } from '@project-serum/anchor';
 import { TruesightProtocol } from '../target/types/truesight_protocol';
+import assert from 'assert';
 
 describe('truesight_protocol', () => {
 
+  // Use a local provider.
+  const provider = anchor.Provider.local();
+
   // Configure the client to use the local cluster.
-  anchor.setProvider(anchor.Provider.env());
+  anchor.setProvider(provider);
 
   const program = anchor.workspace.TruesightProtocol as Program<TruesightProtocol>;
 
   it('Is initialized!', async () => {
     // Add your test here.
-    const tx = await program.rpc.initialize({});
-    console.log("Your transaction signature", tx);
+    const predictionAccount = anchor.web3.Keypair.generate();
+
+    await program.rpc.initialize("truesight", {
+      accounts: {
+        predictionAccount: predictionAccount.publicKey,
+        user: provider.wallet.publicKey,
+        systemProgram: anchor.web3.SystemProgram.programId,
+      },
+      signers: [predictionAccount]
+    });
+
+    const predictionAccountData = await program.account.predictionAccount.fetch(predictionAccount.publicKey);
+
+    assert(predictionAccountData.direction.eq(new anchor.BN(1)))
+    assert(predictionAccountData.asset == "truesight")
   });
 });
