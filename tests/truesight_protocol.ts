c0e91e660e354a67c752bc50b7e9562a1938a6d4
garyjob <garyjob@gmail.com>
2022-03-30 17:55:10 -0700

    integrated pyth.network

--- tests/truesight_protocol.ts
+++ tests/truesight_protocol.ts
@@ -5,27 +5,51 @@ import assert from 'assert';
 
 describe('truesight_protocol', () => {
 
-  // Use a local provider.
-  const provider = anchor.Provider.local();
+  // Pyth Test Accounts on DevNet - TSLA
+  //    https://pyth.network/markets/?cluster=devnet#Equity.US.TSLA/USD
 
-  // Configure the client to use the local cluster.
+  const SolSymbolAccount = new anchor.web3.PublicKey("GaBJpKtnyUbyKe34XuyegR7W98a9PT5cg985G974NY8R");
+  const SolPriceAccount = new anchor.web3.PublicKey("9TaWcpX3kdfdWQQdNtAjW12fNEKdiicmVXuourqn3xJh");  
+
+  // Use the defined cluster - change in Anchor.toml 
+  const provider = anchor.Provider.env();
   anchor.setProvider(provider);
 
+  // Configure the client to use the local cluster.
+  // const provider = anchor.Provider.local();  
+  // anchor.setProvider(provider);
+
   const program = anchor.workspace.TruesightProtocol as Program<TruesightProtocol>;
+
+  const printPredicitonRecord = (recordPublicKey, predictionRecordData) => {
+    console.log("Prediction Record Account: " + recordPublicKey);
+    console.log(predictionRecordData);
+    console.log("entryPrice: " + predictionRecordData.entryPrice.toNumber());
+
+    let expiryDate = new Date(predictionRecordData.expiryDate.toNumber());
+    console.log("expiryDate: " + expiryDate);
+    console.log("validationPrice: " + predictionRecordData.validationPrice.toNumber());
+
+    let validationDate = new Date(predictionRecordData.validationDate.toNumber());
+    console.log("validationDate: " + validationDate);    
+  }
 
   describe('CreatePrediction', () => {
 
     it('creates prediction', async () => {
-      const predictionRecord = anchor.web3.Keypair.generate();
-      const holdoutPeriodSec = 100;
+
+      let predictionRecord = anchor.web3.Keypair.generate();
+      let holdoutPeriodSec = 100;
+      let direction = "UP";
       
       await program.rpc.createPrediction(
-        "truesight_protocol", 
-        new anchor.BN(11), 
+        direction, 
         new anchor.BN(holdoutPeriodSec), 
         {
           accounts: {
             predictionRecord: predictionRecord.publicKey,
+            assetRecord: SolSymbolAccount,
+            assetPriceRecord: SolPriceAccount,
             user: provider.wallet.publicKey,
             systemProgram: anchor.web3.SystemProgram.programId,
           },
@@ -33,23 +57,28 @@ describe('truesight_protocol', () => {
         }
       );
 
-      const predictionRecordData = await program.account.predictionRecord.fetch(predictionRecord.publicKey);
-      assert(predictionRecordData.direction.eq(new anchor.BN(11)));
-      assert(predictionRecordData.asset == "truesight_protocol");
+      let predictionRecordData = await program.account.predictionRecord.fetch(predictionRecord.publicKey);
+      assert(predictionRecordData.direction == "UP");
+      assert(predictionRecordData.asset == "Equity.US.TSLA/USD");
       assert(predictionRecordData.validationDate.toNumber() == 0);
+      assert(predictionRecordData.entryPrice > 0);
+      assert(predictionRecordData.pythPricePublicKey == "9TaWcpX3kdfdWQQdNtAjW12fNEKdiicmVXuourqn3xJh");
+      printPredicitonRecord(predictionRecord.publicKey, predictionRecordData);
     });
 
     it('creates prediction with entry_price set', async () => {
-      const predictionRecord = anchor.web3.Keypair.generate();
-      const holdoutPeriodSec = 100;    
+      let predictionRecord = anchor.web3.Keypair.generate();
+      let holdoutPeriodSec = 100;    
+      let direction = "UP";
       
       await program.rpc.createPrediction(
-        "truesight_protocol", 
-        new anchor.BN(11), 
+        direction, 
         new anchor.BN(holdoutPeriodSec), 
         {
           accounts: {
             predictionRecord: predictionRecord.publicKey,
+            assetRecord: SolSymbolAccount,
+            assetPriceRecord: SolPriceAccount,
             user: provider.wallet.publicKey,
             systemProgram: anchor.web3.SystemProgram.programId,
           },
@@ -57,24 +86,27 @@ describe('truesight_protocol', () => {
         }
       );
 
-      const predictionRecordData = await program.account.predictionRecord.fetch(predictionRecord.publicKey);
-      assert(predictionRecordData.direction.eq(new anchor.BN(11)));
-      assert(predictionRecordData.asset == "truesight_protocol");
+      let predictionRecordData = await program.account.predictionRecord.fetch(predictionRecord.publicKey);
+      assert(predictionRecordData.direction == "UP");
+      assert(predictionRecordData.asset == "Equity.US.TSLA/USD");
       assert(predictionRecordData.validationDate.toNumber() == 0);
-      assert(predictionRecordData.entry_price == 100);
+      assert(predictionRecordData.entryPrice > 0);
+      assert(predictionRecordData.pythPricePublicKey == "9TaWcpX3kdfdWQQdNtAjW12fNEKdiicmVXuourqn3xJh");
     });
 
     it('does not create prediction when holdout period is invalid', async () => {
-      const predictionRecord = anchor.web3.Keypair.generate();
-      const holdoutPeriodSec = 0;
+      let predictionRecord = anchor.web3.Keypair.generate();
+      let holdoutPeriodSec = 0;
+      let direction = "UP";
       
       await program.rpc.createPrediction(
-        "truesight_protocol", 
-        new anchor.BN(11), 
+        direction, 
         new anchor.BN(holdoutPeriodSec), 
         {
           accounts: {
             predictionRecord: predictionRecord.publicKey,
+            assetRecord: SolSymbolAccount,
+            assetPriceRecord: SolPriceAccount,
             user: provider.wallet.publicKey,
             systemProgram: anchor.web3.SystemProgram.programId,
           },
@@ -82,9 +114,8 @@ describe('truesight_protocol', () => {
         }
       );
 
-      const predictionRecordData = await program.account.predictionRecord.fetch(predictionRecord.publicKey);
-      assert(predictionRecordData.direction.toNumber() == 0);
-      assert(predictionRecordData.asset != "truesight_protocol");
+      let predictionRecordData = await program.account.predictionRecord.fetch(predictionRecord.publicKey);
+      assert(predictionRecordData.asset != "Equity.US.TSLA/USD");
       assert(predictionRecordData.validationDate.toNumber() == 0);
     });
 
@@ -93,16 +124,18 @@ describe('truesight_protocol', () => {
   describe('ValidatePrediction', () => {
 
     it('validates prediction', async () => {
-      const predictionRecord = anchor.web3.Keypair.generate();
-      const holdoutPeriodSec = 5;
-      
+      let predictionRecord = anchor.web3.Keypair.generate();
+      let holdoutPeriodSec = 5;
+      let direction = "UP";
+
       await program.rpc.createPrediction(
-        "truesight_protocol", 
         new anchor.BN(11), 
         new anchor.BN(holdoutPeriodSec),        
         {
           accounts: {
             predictionRecord: predictionRecord.publicKey,
+            assetRecord: SolSymbolAccount,
+            assetPriceRecord: SolPriceAccount,
             user: provider.wallet.publicKey,
             systemProgram: anchor.web3.SystemProgram.programId,
           },
@@ -114,28 +147,44 @@ describe('truesight_protocol', () => {
       await program.rpc.validatePrediction({
         accounts: {
           predictionRecord: predictionRecord.publicKey,
+          assetRecord: SolSymbolAccount,
+          assetPriceRecord: SolPriceAccount,
           user: provider.wallet.publicKey,
           systemProgram: anchor.web3.SystemProgram.programId,
         }
       });
 
-      const predictionRecordData = await program.account.predictionRecord.fetch(predictionRecord.publicKey);
-      assert(predictionRecordData.isCorrect == true);
+      let predictionRecordData = await program.account.predictionRecord.fetch(predictionRecord.publicKey);
       assert(predictionRecordData.validationDate.toNumber() * 1000 < Date.now());
+      
+      console.log("Entry price (" +predictionRecordData.entryPrice + ") versus validation price (" + predictionRecordData.validationPrice + ")")
+      if(predictionRecordData.entryPrice > predictionRecordData.validationPrice) {
+        console.log("Predicted up and Price went down")
+        assert(predictionRecordData.isCorrect == false);
+
+      } else if (predictionRecordData.entryPrice < predictionRecordData.validationPrice) {
+        console.log("Predicted up and Price went up")        
+        assert(predictionRecordData.isCorrect == true);
+      } else {
+        console.log("Price has not changed");
+        assert(predictionRecordData.isCorrect == false);
+      }
 
     });
 
     it('does not validate prediction if original prediction was invalid', async () => {
       const predictionRecord = anchor.web3.Keypair.generate();
       const holdoutPeriodSec = 0;
+      let direction = "UP";
       
       await program.rpc.createPrediction(
-        "truesight_protocol", 
         new anchor.BN(11), 
         new anchor.BN(holdoutPeriodSec),        
         {
           accounts: {
             predictionRecord: predictionRecord.publicKey,
+            assetRecord: SolSymbolAccount,
+            assetPriceRecord: SolPriceAccount,
             user: provider.wallet.publicKey,
             systemProgram: anchor.web3.SystemProgram.programId,
           },
@@ -147,13 +196,15 @@ describe('truesight_protocol', () => {
       await program.rpc.validatePrediction({
         accounts: {
           predictionRecord: predictionRecord.publicKey,
+          assetRecord: SolSymbolAccount,
+          assetPriceRecord: SolPriceAccount,
           user: provider.wallet.publicKey,
           systemProgram: anchor.web3.SystemProgram.programId,
         }
       });
 
       const predictionRecordData = await program.account.predictionRecord.fetch(predictionRecord.publicKey);
-      assert(predictionRecordData.asset != "truesight_protocol");      
+      assert(predictionRecordData.asset != "Equity.US.TSLA/USD");
       assert(predictionRecordData.isCorrect == false);
       assert(predictionRecordData.validationDate.toNumber() == 0);
 
@@ -162,14 +213,16 @@ describe('truesight_protocol', () => {
     it('does not validate prediction if expiry date is still in the future', async () => {
       const predictionRecord = anchor.web3.Keypair.generate();
       const holdoutPeriodSec = 5;
-      
+      let direction = "UP";
+
       await program.rpc.createPrediction(
-        "truesight_protocol", 
-        new anchor.BN(11), 
+        direction, 
         new anchor.BN(holdoutPeriodSec),      
         {
           accounts: {
             predictionRecord: predictionRecord.publicKey,
+            assetRecord: SolSymbolAccount,
+            assetPriceRecord: SolPriceAccount,
             user: provider.wallet.publicKey,
             systemProgram: anchor.web3.SystemProgram.programId,
           },
@@ -177,19 +230,20 @@ describe('truesight_protocol', () => {
         }
       );
 
-      await new Promise((r) => setTimeout(r, 4000));
-
       await program.rpc.validatePrediction({
         accounts: {
           predictionRecord: predictionRecord.publicKey,
+          assetPriceRecord: SolPriceAccount,
           user: provider.wallet.publicKey,
           systemProgram: anchor.web3.SystemProgram.programId,
         }
       });
 
-      const predictionRecordData = await program.account.predictionRecord.fetch(predictionRecord.publicKey);     
+      let predictionRecordData = await program.account.predictionRecord.fetch(predictionRecord.publicKey);     
       assert(predictionRecordData.isCorrect == false);
       assert(predictionRecordData.validationDate.toNumber() == 0);
+
+      printPredicitonRecord(predictionRecordData);
 
     });
 
