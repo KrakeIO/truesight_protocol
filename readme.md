809327af191a3e96a148857f90697564d38e1dcb
garyjob <garyjob@gmail.com>
2022-04-09 07:54:03 +0200

    abstracted test account details into their own account

--- readme.md
+++ readme.md
@@ -43,7 +43,37 @@ cargo install --git https://github.com/project-serum/anchor --tag v0.23.0 anchor
 
 #### Node based dependencies
 ```
+npm i -D @types/node
+```
+
+```
 yarn install
+```
+
+## Wallet Environment Setup
+
+Test Player Wallet
+```
+solana-keygen new --outfile "player.json"
+
+# Public Key - 6F3zXV4Kic7VJwvctojfeskyFDP24n9cQL5BEg7YFvJT
+# Private Key location - tests/test_wallets/player.json
+```
+
+Test Betting Pool Wallet
+```
+solana-keygen new --outfile "betting_pool.json"
+
+# Public Key - Ha6KdoiEVc4veZGkJmkhRZnMTayseWnNEd94aekov1Au
+# Private Key location - tests/test_wallets/betting_pool.json
+```
+
+Test Prize Pool Wallet
+```
+solana-keygen new --outfile "prize_pool.json"
+
+# Public Key - 25BWqDfqF7a65hJC833HgP6k2BFJJTgUKPEZya4NDJ84
+# Private Key location - tests/test_wallets/prize_pool.json
 ```
 
 #### Link your own solana wallet
--- tests/truesight_protocol.ts
+++ tests/truesight_protocol.ts
@@ -1,30 +1,17 @@
+import { 
+  SolSymbolAccount, SolPriceAccount, 
+  TSDMintAccount, TokenProgramAccountID, 
+  TestAccount, TestAccountTokenWallet, 
+  BettingPool, BettingPoolTokenAccount, 
+  PrizePool, PrizePoolTokenAccount 
+} from './test_accounts';
+
 import * as anchor from '@project-serum/anchor';
 import { Program } from '@project-serum/anchor';
 import { TruesightProtocol } from '../target/types/truesight_protocol';
 import assert from 'assert';
 
 describe('truesight_protocol', () => {
-
-  // Pyth Test Accounts on DevNet - TSLA
-  //    https://pyth.network/markets/?cluster=devnet#Equity.US.TSLA/USD
-
-  const SolSymbolAccount  = new anchor.web3.PublicKey("GaBJpKtnyUbyKe34XuyegR7W98a9PT5cg985G974NY8R");
-  const SolPriceAccount   = new anchor.web3.PublicKey("9TaWcpX3kdfdWQQdNtAjW12fNEKdiicmVXuourqn3xJh");  
-
-  const TSDMintAccount          = new anchor.web3.PublicKey("dUxFDBEsiDHcWULa6Zr9cDHJHg8uy1PAH69aY74oXia");  
-  const TokenProgramAccountID   = "TokenkegQfeZyiNwAJbNbGKPFXCWuBvf9Ss623VQ5DA";
-
-  // Account used for testing
-  const TestAccount             = new anchor.web3.PublicKey("5iSkxWSbBM3nDYg8T85zCVXSD9baRoDRZuweqxDdYmUY");  
-  const TestAccountTokenWallet  = new anchor.web3.PublicKey("9iyp4DrLuDp2RZrNtzMb1s5FL2qcNcEjHwAYyJY7k4nm");  
-
-  // Account where all bids (TSD) are stored
-  const BettingPool             = new anchor.web3.PublicKey("7fHHgY6Rpx63ancGYJKUgtQ6JdzQ3SuLj991KvqHmZu5");  
-  const BettingPoolTokenAccount = new anchor.web3.PublicKey("J8NFzW9c1R9PwVfD7W8cW61VUJEzPb3TrykwwoWZPjwJ");
-
-  // Account where accumulated TSDs from previous loses and DAO contributions are stored
-  const PrizePool               = new anchor.web3.PublicKey("5Bbk3FGwXzLCbPSoiHYtHBwsBfYqndBY8cCg5r3xedvy");  
-  const PrizePoolTokenAccount   = new anchor.web3.PublicKey("CyTUhGP9DWYWAAsZJW5J75RutKiMhnCTCMv7bprEDz8a");  
 
   // Use the defined cluster - change in Anchor.toml 
   const provider = anchor.Provider.env();
