75b9149e94f471e4baf95f5d6514057d61c8400a
dhruvkratos <dhruv.jain@kratosinnovationlabs.com>
2022-06-06 19:19:46 +0530

    create a TSD token mint and check the direction in the program

--- programs/pyth_stake/src/lib.rs
+++ programs/pyth_stake/src/lib.rs
@@ -1,47 +1,48 @@
 use anchor_lang::prelude::*;
 use pyth_sdk_solana::{load_price_feed_from_account_info, PriceFeed};
+use anchor_spl::token::{Mint, Token, TokenAccount, Transfer};
 
 declare_id!("2iFXrFurDAZwv5HZHebiTgE4p2GoKkokw8F3fjKnPy4s");
+
+const PREDICTION_SEED: &'static [u8] = b"prediction";
+const POOL_SEED: &'static [u8] = b"betting_pool";
 
 #[program]
 pub mod pyth_stake {
     use super::*;
 
-    pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
-
-        let prediction_record   = &mut ctx.accounts.base_account;
+    pub fn create_prediction(ctx: Context<CreatePrediction>, asset_name: String, predicted_price: i64, holdout_period_sec: u64, bid_amount: u64) -> Result<()> {
 
         let price_account_info = &mut ctx.accounts.asset_price_record;
-        let price_feed = load_price_feed_from_account_info( &price_account_info ).unwrap();
-        let current_price = price_feed.get_current_price().unwrap();
-        msg!("{}", current_price.price);
-        prediction_record.asset_record = current_price.price.to_string();
+        // let price_feed = load_price_feed_from_account_info( &price_account_info ).unwrap();
+        // let current_price = price_feed.get_current_price().unwrap();
 
-            // Fetch product information from Pyth.Network
-            // let pyth_product                = &ctx.accounts.asset_record;
-            // let pyth_product_data           = &pyth_product.try_borrow_data()?;
-            // let product_account: Product    = *load_product(pyth_product_data).unwrap();
-            
-            // // Fetch price information from Pyth.Network
-            // let pyth_price_info = &ctx.accounts.asset_price_record;
-            // let pyth_price_data = &pyth_price_info.try_borrow_data()?;
-            // let price_account: Price = *load_price(pyth_price_data).unwrap();
-            
-            // for (key, val) in product_account.iter() {
-            //     if key == "symbol" {
-            //         msg!(&val.to_string());
-            //         // prediction_record.asset = val.to_string();
+        // if current_price.price > predicted_price {
+        //     ctx.accounts.base_account.direction = String::from("DOWN");
+        // }
+        // else {
+        //     ctx.accounts.base_account.direction = String::from("UP");
+        // }
 
-            //     }
-            // }
+        ctx.accounts.base_account.asset                     = asset_name;
+        // ctx.accounts.base_account.is_correct                = false;
+        // ctx.accounts.base_account.expiry_date               = (holdout_period_sec as i64) + Clock::get().unwrap().unix_timestamp;
+        // // ctx.accounts.base_account.bidder_token_wallet_key   = ctx.accounts.wallet_to_withdraw_from.key();
+        // ctx.accounts.base_account.pyth_product_public_key   = ctx.accounts.asset_record.key();
+        // ctx.accounts.base_account.pyth_price_public_key     = ctx.accounts.asset_price_record.key();
+        // ctx.accounts.base_account.entry_price               = current_price.price;
+        // ctx.accounts.base_account.validation_price          = predicted_price;
+        // ctx.accounts.base_account.bid_amount                = bid_amount;
+
 
         Ok(())
     }
 }
 
 #[derive(Accounts)]
-pub struct Initialize<'info> {
-    #[account(init, payer = authority, space = 800)]
+#[instruction(asset_name: String)]
+pub struct CreatePrediction<'info> {
+    #[account(init, payer = authority, seeds = [PREDICTION_SEED, asset_name.as_ref(), authority.key().as_ref()], bump, space = 800)]
     pub base_account: Account<'info, PredictionRecord>,
     #[account(mut)]
     pub authority: Signer<'info>,
@@ -51,10 +52,23 @@ pub struct Initialize<'info> {
     #[account(mut)]
     /// CHECK:
     pub asset_price_record: AccountInfo<'info>,
+    // pub wallet_to_withdraw_from: Account<'info, TokenAccount>,
     pub system_program: Program<'info, System>
 }
 
 #[account]
 pub struct PredictionRecord {
-    pub asset_record: String
+    pub direction: String, // 4
+    pub asset: String, // 15
+    pub is_correct: bool, // 1
+    pub expiry_date: i64, 
+    pub bidder_token_wallet_key: Pubkey, // 32
+    pub pyth_product_public_key: Pubkey, // 32
+    pub pyth_price_public_key: Pubkey, // 32
+    pub validation_date: i64,
+    pub entry_price: i64,
+    // pub entry_expo: i32,
+    pub validation_price: i64,
+    // pub validation_expo: i32,
+    pub bid_amount: u64, // 8
 }
--- tests/pyth_stake.ts
+++ tests/pyth_stake.ts
@@ -3,6 +3,8 @@ import { Program } from "@project-serum/anchor";
 import { publicKey } from "@project-serum/anchor/dist/cjs/utils";
 import { PythStake } from "../target/types/pyth_stake";
 import {PublicKey} from '@solana/web3.js'
+import * as spl from "@solana/spl-token";
+const assert = require("assert");
 
 describe("pyth_stake", () => {
   // Configure the client to use the local cluster.
@@ -13,8 +15,14 @@ describe("pyth_stake", () => {
 
   const assetRecord = new PublicKey("3m1y5h2uv7EQL3KaJZehvAJa4yDNvgc5yAdL9KPMKwvk");
   const assetPriceRecord = new PublicKey("HovQMDrbAgAYPCmHVSrezcSmkMtXSSUsLDFANExrZh2J");
+  const assetName = "BTCUSD";
 
   const alice = anchor.web3.Keypair.generate();
+
+  let TSDMint: anchor.web3.PublicKey;
+  let aliceTokenAccount: anchor.web3.PublicKey; 
+
+  let initialMintAmount = 100000000;
 
   it("is wallet funded", async () => {
 
@@ -22,26 +30,77 @@ describe("pyth_stake", () => {
       await provider.connection.requestAirdrop(alice.publicKey, 10000000),
       "confirmed"
     );
+
+    const aliceUserBalance = await provider.connection.getBalance(
+      alice.publicKey
+    );
+
+    assert.strictEqual(10000000000, aliceUserBalance);
+
+  })
+
+  it("creates TSD token and token account", async() => {
+
+    TSDMint = await spl.createMint(
+      provider.connection,
+      alice,
+      alice.publicKey,
+      null,
+      6
+    );
+
+    aliceTokenAccount = await spl.createAccount(
+      provider.connection,
+      alice,
+      TSDMint,
+      alice.publicKey
+    );
+
+    await spl.mintTo(
+      provider.connection,
+      alice,
+      TSDMint,
+      aliceTokenAccount,
+      alice.publicKey,
+      initialMintAmount,
+      [alice]
+    );
+
+    let _aliceTokenAccount = await spl.getAccount(
+      provider.connection,
+      aliceTokenAccount
+    );
+
+    assert.equal(initialMintAmount, _aliceTokenAccount.amount);
+
   })
   
 
-  it("Is initialized!", async () => {
+  it("creates prediction!", async () => {
     // Add your test here.
     const aliceUserBalance = await provider.connection.getBalance(
       alice.publicKey
     );
     console.log(aliceUserBalance)
+
+    const holdoutPeriod = 100;
+    const bidAmount = 1000;
+
+    const [predictionPDA, predictionBump] = await anchor.web3.PublicKey.findProgramAddress(
+      [Buffer.from("prediction"), Buffer.from(assetName), alice.publicKey.toBuffer()],
+      program.programId
+    )
     
-    const tx = await program.methods.initialize().accounts({
-      baseAccount: alice.publicKey,
+    const tx = await program.methods.createPrediction(assetName, new anchor.BN(3238769000000), new anchor.BN(holdoutPeriod), new anchor.BN(bidAmount)).accounts({
+      baseAccount: predictionPDA,
       authority: alice.publicKey,
       assetRecord: assetRecord,
       assetPriceRecord: assetPriceRecord,
       systemProgram: anchor.web3.SystemProgram.programId
     }).signers([alice]).rpc();
 
-    const state = await program.account.predictionRecord.fetch(alice.publicKey);
-    console.log(state.assetRecord)
+    const state = await program.account.predictionRecord.fetch(predictionPDA);
+    console.log(state.asset)
 
     console.log("Your transaction signature", tx);
   });
